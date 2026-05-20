0a4463fd12e0937ab2b14a21febeab4b03565df7
Ot-811 <omtr0904@gmail.com>
2026-05-20 19:36:06 +0530

    final touches

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ while True:
     bbox = mask_.getbbox()
     if bbox is not None:
         x1, y1, x2, y2 = bbox
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
     
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
