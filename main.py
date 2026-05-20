f234d4488f99a2ac220e48b16223165d84eb5ae6
Ot-811 <omtr0904@gmail.com>
2026-05-20 19:08:02 +0530

    added mask and bbox

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import os
 import cv2
 import numpy as np
+from PIL import Image
+from utility import get_limits
+
+color = (0, 255, 255)
+lowerLimit, upperLimit = get_limits(color)
 
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lowerLimit, upperLimit)
+    mask_ = Image.fromarray(mask)
+    bbox = mask_.getbbox()
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- utility.py
+++ utility.py
@@ -3,7 +3,7 @@ import cv2
 
 def get_limits(color):
     
-    c = cv2.uint8([[color]])
+    c = np.uint8([[color]])
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
     lowerLimit = hsvC[0][0][0] - 10, 100, 100
     upperLimit = hsvC[0][0][0] + 10, 255, 255
