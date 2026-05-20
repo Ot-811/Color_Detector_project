e962702fc8e9dd77aa8195e7bdd6a7ecb559b5d5
Ot-811 <omtr0904@gmail.com>
2026-05-20 18:42:34 +0530

    flipped the camera

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
-
+    frame = cv2.flip(frame, 1)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
